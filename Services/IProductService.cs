026ad9648aea57ea6dfd2a83d746daf414527e04
Sishir Sharma <sisirux@gmail.com>
2023-11-12 14:58:05 +1100

    Use app svc connection string

---
+++ Services/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using sqlapp.Models;
+
+namespace sqlapp.Pages.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+    }
+}
--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -3,22 +3,17 @@ using System.Data.SqlClient;
 
 namespace sqlapp.Pages.Services
 {
-    public class ProductService
+    public class ProductService : IProductService
     {
-        private static string db_source = "prac-learn-11-dbserver.database.windows.net";
-        private static string db_user = "sqladmin";
-        private static string db_password = "BigChungu$11";
-        private static string db_database = "appdb";
+        private readonly IConfiguration _configuration;
 
+        public ProductService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         private SqlConnection GetConnection()
         {
-            var _builder = new SqlConnectionStringBuilder();
-            _builder.DataSource = db_source;
-            _builder.UserID = db_user;
-            _builder.Password = db_password;
-            _builder.InitialCatalog = db_database;
-
-            return new SqlConnection(_builder.ConnectionString);
+            return new SqlConnection(_configuration.GetConnectionString("appdb"));
         }
 
         public List<Product> GetProducts()
